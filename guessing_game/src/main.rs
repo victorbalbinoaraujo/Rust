60c4a9e230e56c823233a49b475745db47ca5b01
Victor Balbino Araujo <victorbalbinoaraujo1@outlook.com>
2022-09-26 06:52:27 -0300

    Cargo fmt & clippy

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,6 +1,6 @@
-use std::io;
-use std::cmp::Ordering;
 use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
     println!("Guess the number!");
@@ -9,23 +9,24 @@ fn main() {
 
     loop {
         println!("Please input your guess");
-        
+
         let mut guess: String = String::new();
 
-        io::stdin().read_line(&mut guess)
-                   .expect("Failed to read line");
-        
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue
+            Err(_) => continue,
         };
 
         println!("You guessed: {guess}");
 
         match guess.cmp(&secret_number) {
-            Ordering::Less    => println!("Too small!"),
+            Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
-            Ordering::Equal   => {
+            Ordering::Equal => {
                 println!("You win!");
                 break;
             }
