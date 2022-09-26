31df2addb360eb0a212dc3175b8d6a532748bcb6
Victor Balbino Araujo <victorbalbinoaraujo1@outlook.com>
2022-09-26 06:53:15 -0300

    Cargo fmt & clippy

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -1,14 +1,14 @@
 use std::io;
 
 fn main() {
-    
     println!("Temperature");
 
     let mut temperature: String = String::new();
 
-    io::stdin().read_line(&mut temperature)
-               .expect("Failed to read temperature");
-    
+    io::stdin()
+        .read_line(&mut temperature)
+        .expect("Failed to read temperature");
+
     let temperature: i32 = match temperature.trim().parse() {
         Ok(temperature) => temperature,
         Err(_e) => {
@@ -20,13 +20,14 @@ fn main() {
 
     let mut unit_temp: String = String::new();
 
-    io::stdin().read_line(&mut unit_temp)
-               .expect("Failed to read unit of temperature");
-    
+    io::stdin()
+        .read_line(&mut unit_temp)
+        .expect("Failed to read unit of temperature");
+
     match unit_temp.as_str() {
         "C\r\n" => println!("{}", f_to_c(temperature)),
         "F\r\n" => println!("{}", c_to_f(temperature)),
-        _       => println!("Invalid unit of temperature!"),
+        _ => println!("Invalid unit of temperature!"),
     }
 }
 
