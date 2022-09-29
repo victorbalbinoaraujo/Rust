444f200877fa9cec69e0a9fdee9307e13889eae0
Victor Balbino Araujo <victorbalbinoaraujo1@outlook.com>
2022-09-28 22:06:23 -0300

    Exercism - Lucian's Luscious Lasagna

--- exercism/lucians_luscious_lasagna/src/main.rs
+++ exercism/lucians_luscious_lasagna/src/main.rs
@@ -1,18 +1,17 @@
-fn expected_minutes_in_oven() -> i32 {
+pub fn expected_minutes_in_oven() -> i32 {
     40
 }
 
-fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
+pub fn remaining_minutes_in_oven(actual_minutes_in_oven: i32) -> i32 {
     expected_minutes_in_oven() - actual_minutes_in_oven
 }
 
-fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
+pub fn preparation_time_in_minutes(number_of_layers: i32) -> i32 {
     number_of_layers * 2
 }
 
-fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
-    (expected_minutes_in_oven() - remaining_minutes_in_oven(actual_minutes_in_oven))
-        + preparation_time_in_minutes(number_of_layers)
+pub fn elapsed_time_in_minutes(number_of_layers: i32, actual_minutes_in_oven: i32) -> i32 {
+   actual_minutes_in_oven + preparation_time_in_minutes(number_of_layers)
 }
 
 fn main() {
